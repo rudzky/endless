3bcc4921d9711c995c656d3928c75c58b997a2b6
Rudzky <lubieprogramowanie@gmail.com>
2020-06-20 19:04:50 +0200

    Updates

---
+++ precache-manifest.123da75ed81ff98e1ecc7f5091830083.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1dbe3019cf6c5edde4b6cc435d0094",
+    "url": "/endless/index.html"
+  },
+  {
+    "revision": "58087a14a100ebbfdf03",
+    "url": "/endless/static/css/2.fbc75d8e.chunk.css"
+  },
+  {
+    "revision": "b1e427718c95f90db3d3",
+    "url": "/endless/static/css/main.c0ce89a3.chunk.css"
+  },
+  {
+    "revision": "58087a14a100ebbfdf03",
+    "url": "/endless/static/js/2.ff0cda3c.chunk.js"
+  },
+  {
+    "revision": "e86c255877b27f51ba8be4c82900e380",
+    "url": "/endless/static/js/2.ff0cda3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e427718c95f90db3d3",
+    "url": "/endless/static/js/main.e0fd98b4.chunk.js"
+  },
+  {
+    "revision": "7ffd7fc701aa2e16afce",
+    "url": "/endless/static/js/runtime-main.7e41e615.js"
+  },
+  {
+    "revision": "263293c65cc2f4b6f673f423898d2f0b",
+    "url": "/endless/static/media/CircularStd-Black.263293c6.svg"
+  },
+  {
+    "revision": "59880bb792378f77deca117ea6b17567",
+    "url": "/endless/static/media/CircularStd-Black.59880bb7.woff"
+  },
+  {
+    "revision": "c885948f6112a185e0e6c79963826882",
+    "url": "/endless/static/media/CircularStd-Black.c885948f.ttf"
+  },
+  {
+    "revision": "faf6412f40aa21da14fc685a0a610d6e",
+    "url": "/endless/static/media/CircularStd-Black.faf6412f.eot"
+  },
+  {
+    "revision": "2d00dacfa45682b454cca077d60a6f00",
+    "url": "/endless/static/media/CircularStd-BlackItalic.2d00dacf.eot"
+  },
+  {
+    "revision": "7f645a713b0b95ae78201587d97e7f47",
+    "url": "/endless/static/media/CircularStd-BlackItalic.7f645a71.ttf"
+  },
+  {
+    "revision": "e13b5c7b607cba29c792f13d5328ddb6",
+    "url": "/endless/static/media/CircularStd-BlackItalic.e13b5c7b.woff"
+  },
+  {
+    "revision": "f3c01bca549b7dfca0aed37ca540c016",
+    "url": "/endless/static/media/CircularStd-BlackItalic.f3c01bca.svg"
+  },
+  {
+    "revision": "1ced22ee6d45bdd05a5383fd9dbd1b43",
+    "url": "/endless/static/media/CircularStd-Bold.1ced22ee.woff"
+  },
+  {
+    "revision": "b4c1f01a6535abf9385215cfa92277e5",
+    "url": "/endless/static/media/CircularStd-Bold.b4c1f01a.svg"
+  },
+  {
+    "revision": "ce2a6c4154de87815e8971d21a987403",
+    "url": "/endless/static/media/CircularStd-Bold.ce2a6c41.ttf"
+  },
+  {
+    "revision": "e66cd4391be238164ba3fd7954ddddc4",
+    "url": "/endless/static/media/CircularStd-Bold.e66cd439.eot"
+  },
+  {
+    "revision": "8f682a90d0d1ded2f43b1364af783fc8",
+    "url": "/endless/static/media/CircularStd-BoldItalic.8f682a90.woff"
+  },
+  {
+    "revision": "91e639327f684581d17a0a8b9cc64737",
+    "url": "/endless/static/media/CircularStd-BoldItalic.91e63932.svg"
+  },
+  {
+    "revision": "e714c355d6f6687fc2a03019c348fe53",
+    "url": "/endless/static/media/CircularStd-BoldItalic.e714c355.eot"
+  },
+  {
+    "revision": "fe0bddb876ee3c18c1d99fdbabb6e20c",
+    "url": "/endless/static/media/CircularStd-BoldItalic.fe0bddb8.ttf"
+  },
+  {
+    "revision": "81002b22dbdd8b4fda534063cf6eaea6",
+    "url": "/endless/static/media/CircularStd-Book.81002b22.svg"
+  },
+  {
+    "revision": "c43e9feb9ca817ae86afb47fd0ee4f94",
+    "url": "/endless/static/media/CircularStd-Book.c43e9feb.ttf"
+  },
+  {
+    "revision": "d246520db4ce4615016723d4828da9bc",
+    "url": "/endless/static/media/CircularStd-Book.d246520d.eot"
+  },
+  {
+    "revision": "ed76eb21560f0dc038cce0adfd65ea3c",
+    "url": "/endless/static/media/CircularStd-Book.ed76eb21.woff"
+  },
+  {
+    "revision": "61889cfbb19d3a736ed1918d40c2b7ed",
+    "url": "/endless/static/media/CircularStd-BookItalic.61889cfb.woff"
+  },
+  {
+    "revision": "6d37d850bec0c1c86f9ebdbfbd9c4fc2",
+    "url": "/endless/static/media/CircularStd-BookItalic.6d37d850.eot"
+  },
+  {
+    "revision": "77a3e2ba714882de2159c63b2b6c0f7e",
+    "url": "/endless/static/media/CircularStd-BookItalic.77a3e2ba.ttf"
+  },
+  {
+    "revision": "892039ad6d317870ff355e662bc6f701",
+    "url": "/endless/static/media/CircularStd-BookItalic.892039ad.svg"
+  },
+  {
+    "revision": "3473fea88bf57aa66b797db5e2f03349",
+    "url": "/endless/static/media/CircularStd-Medium.3473fea8.eot"
+  },
+  {
+    "revision": "46d551dfd0caa61f1332d7c477f584c2",
+    "url": "/endless/static/media/CircularStd-Medium.46d551df.ttf"
+  },
+  {
+    "revision": "bb9a359afc17edc2c275b4212b8f2fd2",
+    "url": "/endless/static/media/CircularStd-Medium.bb9a359a.woff"
+  },
+  {
+    "revision": "e6af6d0cc0526e208f4e85d1a0171f82",
+    "url": "/endless/static/media/CircularStd-Medium.e6af6d0c.svg"
+  },
+  {
+    "revision": "1e3f8d0ec2758c561b1d86a34bfd282c",
+    "url": "/endless/static/media/CircularStd-MediumItalic.1e3f8d0e.eot"
+  },
+  {
+    "revision": "21a7520d477ce7c4202f49af5708c586",
+    "url": "/endless/static/media/CircularStd-MediumItalic.21a7520d.woff"
+  },
+  {
+    "revision": "7095d23a432c27d51c271d4df95f9cf7",
+    "url": "/endless/static/media/CircularStd-MediumItalic.7095d23a.ttf"
+  },
+  {
+    "revision": "fde448ffc3e87ce3b18c80d042f352ed",
+    "url": "/endless/static/media/CircularStd-MediumItalic.fde448ff.svg"
+  },
+  {
+    "revision": "acf1ab0546898197de0a983614513378",
+    "url": "/endless/static/media/backTo.acf1ab05.svg"
+  },
+  {
+    "revision": "2210acfead4d2c11a9fbe90fec93c909",
+    "url": "/endless/static/media/ball2.2210acfe.png"
+  },
+  {
+    "revision": "926d45ca59a4e2e2595d36ea03686873",
+    "url": "/endless/static/media/ball3.926d45ca.png"
+  },
+  {
+    "revision": "81d4bfb677e32cb4d8c5ad14ba05e061",
+    "url": "/endless/static/media/ball4.81d4bfb6.png"
+  },
+  {
+    "revision": "626194b0ac26f538b6f86d62968fadc6",
+    "url": "/endless/static/media/happy.626194b0.svg"
+  },
+  {
+    "revision": "cf58ccf7d22f1d9be75572933a014533",
+    "url": "/endless/static/media/heart.cf58ccf7.svg"
+  },
+  {
+    "revision": "a9de3c484ee491cbea85e9666d2f20f4",
+    "url": "/endless/static/media/house.a9de3c48.svg"
+  },
+  {
+    "revision": "20130c2b07ce9050d61922830b1ea6a6",
+    "url": "/endless/static/media/logo-end.20130c2b.svg"
+  },
+  {
+    "revision": "9c953d2e2bcfbcadafdc477164526ec1",
+    "url": "/endless/static/media/love.9c953d2e.svg"
+  },
+  {
+    "revision": "4d835522e4aea5e10b6b50a91beb423e",
+    "url": "/endless/static/media/neutral.4d835522.svg"
+  },
+  {
+    "revision": "151afbea1348c4ea3a68155bc812d9dc",
+    "url": "/endless/static/media/next.151afbea.svg"
+  },
+  {
+    "revision": "562e3ee9b3530fdc8346186ab15e8018",
+    "url": "/endless/static/media/pause-but.562e3ee9.svg"
+  },
+  {
+    "revision": "017939708a71ade8f0f5f423b0d99d6f",
+    "url": "/endless/static/media/play-but.01793970.svg"
+  },
+  {
+    "revision": "afd8cbb9ed3998eb054757a32b02c9c3",
+    "url": "/endless/static/media/roll.afd8cbb9.svg"
+  },
+  {
+    "revision": "7da98e8d6a19306aaf66da1e4388ed47",
+    "url": "/endless/static/media/sad.7da98e8d.svg"
+  },
+  {
+    "revision": "5253c14110dfaa487e7dcdf1a0e011a4",
+    "url": "/endless/static/media/tick.5253c141.svg"
+  },
+  {
+    "revision": "ee1c69347169e0d368b4df32f1db0452",
+    "url": "/endless/static/media/x.ee1c6934.svg"
+  }
+]);
